16d31834117c96205d55bf0d83bc1586b2c10ab9
Tango <maxim.ant07@gmail.com>
2025-09-15 18:45:56 +0300

    Update Light.h

--- src/Light.h
+++ src/Light.h
@@ -13,56 +13,326 @@ class Light
 {
 public:
 	Light() = default;
-	Light(float pIntensity, const glm::vec3& pColorLight, bool pCreateBlockSourceLight);
+	Light(float pIntensity, const glm::vec3& pColorLight);
 	~Light() = default;
 	Light(const Light& pOtherLight) = delete;
 	Light& operator=(const Light& pOtherLight) = delete;
 	Light(Light&& pOtherLight) noexcept;
 	Light& operator=(Light&& pOtherLight) noexcept;
 
-	void init(float pIntensity, const glm::vec3& pColorLight, bool pCreateBlockSourceLight);
+	void init(float pIntensity, const glm::vec3& pColorLight);
 
 	virtual void sendToShader(Shader& pShader) = 0;
 	virtual void sendToShaderArray(Shader& pShader, int32_t pIndex) = 0;
+	
+	virtual void setPosLight(const glm::vec3& pPosLight) = 0;
+	virtual glm::vec3 getPosLight() const noexcept = 0;
+
+	void enableAttenuation(float pConstant, float pLinear, float pQuadratic);
+	void disableAttenuation();
+	bool attenuationIsEnabled() const noexcept;
 
 	void setIntensity(float pIntensity);
 	void setColor(const glm::vec3& pColor);
-	void setVisibilityForBlockSource(bool pVisibility);
 
 	float getIntensity() const noexcept;
 	glm::vec3 getColor() const noexcept;
-	Mesh& getBlockLight();
 
 private:
 	bool mBlockIsVisible{};
 	float mIntensity{};
 	glm::vec3 mColorLight{};
-	std::unique_ptr<Mesh> mBlockLight;
 
+	struct Attenuation
+	{
+		bool mAttenuationIsUp{ false };
+		float mConstant{};
+		float mLinear{};
+		float mQuadratic{};
+	} mAttenuation;
+};
+
+class DirectionalLight : public Light
+{
+public:
+	DirectionalLight() = default;
+
+	// 
+	// without attenuation
+	//
+	DirectionalLight(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pIntensity = 1.0f,
+					 const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	DirectionalLight(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pConstant, float pLinear, float pQuadratic,
+					 float pIntensity = 1.0f,
+					 const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	// 
+	// without attenuation
+	//
+	void init(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	void init(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pConstant, float pLinear, float pQuadratic,
+			  float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void setPosLight(const glm::vec3& pPos) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPathForShader(const std::filesystem::path& pFragmentPath, 
+						  const std::filesystem::path& pVertexPath);
+
+	void setDirection(const glm::vec3& pDirection);
+	glm::vec3 getDirection() const noexcept;
+
+private:
+	glm::vec3 mPos{ 1.0f };
+	glm::vec3 mDirection{ 1.0f };
+
+	Shader mDirectionalLightShader;
 };
 
 class PointLight : public Light
 {
 public:
 	PointLight() = default;
-	PointLight(const glm::vec3& pPosLight, bool pCreateBlockSourceLight, float pIntensity = 1.0f,
-			   const glm::vec3& pColorLight = glm::vec3(1.0f), float pConstant = 1.0f, 
-			   float pLinear = 0.045f, float pQuadratic = 0.075f);
 
-	void init(const glm::vec3& pPosLight, bool pCreateBlockSourceLight, float pIntensity = 1.0f,
-			  const glm::vec3& pColorLight = glm::vec3(1.0f), float pConstant = 1.0f,
-			  float pLinear = 0.045f, float pQuadratic = 0.075f);
-	
+	// 
+	// without attenuation
+	//
+	PointLight(const glm::vec3& pPosLight, float pIntensity = 1.0f,
+			   const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	PointLight(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic,
+			   float pIntensity = 1.0f,
+			   const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	// 
+	// without attenuation
+	//
+	void init(const glm::vec3& pPosLight, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	void init(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic,
+			  float pIntensity = 1.0f,
+		  	  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
 	void sendToShader(Shader& pShader) override;
 	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
 
-	void setPosLight(const glm::vec3& pPosLight);
-	glm::vec3 getPosLight() const noexcept;
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+	void setPathForShader(const std::filesystem::path& pFragmentPath,
+						  const std::filesystem::path& pVertexPath);
 
 private:
-	glm::vec3 mPosLight;
-	float mConstant{};
-	float mLinear{};
-	float mQuadratic{};
+	glm::vec3 mPosLight{ 1.0f };
 
+	Shader mPointLightShader;
+};
+
+class Spotlight : public Light
+{
+public:
+	Spotlight() = default;
+
+	// 
+	// without attenuation
+	//
+	Spotlight(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pRadius, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	Spotlight(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pRadius, 
+			  float pConstant, float pLinear, float pQuadratic, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	// 
+	// without attenuation
+	//
+	void init(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pRadius, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	void init(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pRadius, 
+			  float pConstant, float pLinear, float pQuadratic, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void setRadius(float pRadius);
+	float getRadius() const noexcept;
+
+	void setDirection(const glm::vec3& pDirection);
+	glm::vec3 getDirection() const noexcept;
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+private:
+	float mRadius{ 1.0f };
+
+	glm::vec3 mPosLight{ 1.0f };
+	glm::vec3 mDirection{ 1.0f };
+
+	Shader mSpotLightShader;
+};
+
+class Flashlight : public Spotlight
+{
+public:
+	Flashlight() = default;
+
+	//
+	// u can set attenuation then, via methods in the base class;
+	// InnerCos - the radius of the beginning of the flashlight;
+	// OuterCos - the opposite, the radius of the end of the flashlight;
+	//
+	Flashlight(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pInnerCutOff, float pOuterCutOff,
+			   float pIntensity = 1.0f,
+			   const glm::vec3& pColorLight = glm::vec3(1.0f));
+	
+	//
+	// u can set attenuation then, via methods in the base class;
+	//
+	void init(const glm::vec3& pPosLight, const glm::vec3& pDirection, float pInnerCutOff, float pOuterCutOff, 
+			  float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void setPathForShader(const std::filesystem::path& pFragmentPath,
+						  const std::filesystem::path& pVertexPath);
+
+	void setOuterCos(float pOuterCos);
+	void setInnerCos(float pInnerCos);
+	float getOuterCos() const noexcept;
+	float getInnerCos() const noexcept;
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+private:
+	glm::vec3 mPos{ 1.0f };
+	glm::vec3 mDirection{ 1.0f };
+
+	Shader mFlashlightShader;
+
+	float outerCutOff, innerCutOff;
+};
+
+class AreaLight : public Light
+{
+public:
+	AreaLight() = default;
+
+	// 
+	// without attenuation
+	//
+	AreaLight(const glm::vec3& pPosLight, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	AreaLight(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic,
+			  float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	// 
+	// without attenuation
+	//
+	void init(const glm::vec3& pPosLight, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	// 
+	// with attenuation
+	//
+	void init(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic,
+			  float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+
+	void setPathForShader(const std::filesystem::path& pFragmentPath,
+						  const std::filesystem::path& pVertexPath);
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+private:
+	glm::vec3 mPos{ 1.0f };
+	
+	Shader mAreaLight;
+};
+
+//
+// neon
+//
+class EmissiveLight : public Light
+{
+public:
+	EmissiveLight() = default;
+	EmissiveLight(const glm::vec3& pPosLight, bool pFlashing, float pIntensity = 1.0f,
+				  const glm::vec3& pColorLight = glm::vec3(1.0f));
+	void init(const glm::vec3& pPosLight, bool pFlashing, float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void setFlashing(bool pFlashing);
+	bool isFlashing() const noexcept;
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+private:
+	bool mIsFlashing{ false };
+
+	glm::vec3 mPos{ 1.0f };
+
+	Shader mEmissiveLightShader;
+};
+
+class AttenuationLight : public Light
+{
+public:
+	AttenuationLight() = default;
+
+	AttenuationLight(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic, 
+					 float pIntensity = 1.0f,
+					 const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void init(const glm::vec3& pPosLight, float pConstant, float pLinear, float pQuadratic, 
+			  float pIntensity = 1.0f,
+			  const glm::vec3& pColorLight = glm::vec3(1.0f));
+
+	void sendToShader(Shader& pShader) override;
+	void sendToShaderArray(Shader& pShader, int32_t pIndex) override;
+
+	void setPosLight(const glm::vec3& pPosLight) override;
+	glm::vec3 getPosLight() const noexcept override;
+
+private:
+	glm::vec3 mPos{ 1.0f };
+
+	Shader mAttenuationLightShader;
 };
