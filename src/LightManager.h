bb34f5bc5ea7c2401a1f776cf125550b25f042b1
Tango <maxim.ant07@gmail.com>
2025-09-16 16:03:11 +0300

    Update LightManager.h

--- src/LightManager.h
+++ src/LightManager.h
@@ -16,11 +16,11 @@ public:
 	
 	auto pushLight(std::string_view pNameLight, std::unique_ptr<Light>&& pLight) -> void;
 	auto popLight(std::string_view pNameLight) -> void;
-	auto getLight(std::string_view pNameLight) -> std::expected<std::unique_ptr<Light>&, std::string_view>;
-	auto getStorageLight() const noexcept -> const std::unordered_map<std::string, std::unique_ptr<Light>>;
+	auto getLight(std::string_view pNameLight) -> std::expected<std::reference_wrapper<std::unique_ptr<Light>>, std::string_view>;
+	auto getStorageLight() -> std::unordered_map<std::string, std::unique_ptr<Light>>&;
 	auto getSize() const noexcept -> size_t;
 	
-	auto sendAllToShader(Shader& pShader);
+	auto sendAllToShader(Shader& pShader) -> void;
 
 private:
 	std::unordered_map<std::string, std::unique_ptr<Light>> mStrgLight;
