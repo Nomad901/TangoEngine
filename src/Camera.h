965ae628edb943352860753582aeb8cd84e0bfaf
Tango <maxim.ant07@gmail.com>
2025-09-19 20:40:27 +0300

    Update Camera.h

--- src/Camera.h
+++ src/Camera.h
@@ -13,7 +13,7 @@ public:
 	Camera();
 
 	glm::mat4 getViewMatrix();
-	void mouseMovement(const glm::vec2& pPos);
+	void mouseMovement(const glm::vec2& pPos, bool pConstraintPitch = true);
 	void moveRight(float pSpeed);
 	void moveLeft(float pSpeed);
 	void moveForward(float pSpeed);
@@ -21,11 +21,28 @@ public:
 	void moveDown(float pSpeed);
 	void moveUp(float pSpeed);
 
+	void setSensivity(float pSensivity);
+	float getSensivity() const noexcept;
+
+	void setYaw(float pYaw);
+	void setPitch(float pPitch);
+	float getYaw() const noexcept;
+	float getPitch() const noexcept;
+
+	void setPos(const glm::vec3& pPos);
+	void setDirection(const glm::vec3& pDirection);
+	void setUpVec(const glm::vec3& pUpVec);
 	glm::vec3 getPos() const noexcept;
 	glm::vec3 getDirection() const noexcept;
 	glm::vec3 getUpVec() const noexcept;
 
 private:
+	void updateCameraVertex();
+
+private:
 	glm::vec3 mEye, mDirection, mUpVec;
+
+	float mYaw, mPitch;
+	float mSensivity;
 };
 
