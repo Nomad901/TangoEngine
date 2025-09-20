5071bfccacd51b56439895a9cf85c50b7f8369a8
Tango <maxim.ant07@gmail.com>
2025-09-20 05:34:25 +0300

    Update Camera.h

--- src/Camera.h
+++ src/Camera.h
@@ -7,6 +7,16 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtx/rotate_vector.hpp"
 
+enum class moveSides
+{
+	RIGHT = 0,
+	LEFT = 1,
+	FORWARD = 2,
+	BACKWARD = 3,
+	UP = 4,
+	DOWN = 5
+};
+
 class Camera
 {
 public:
@@ -14,18 +24,14 @@ public:
 
 	glm::mat4 getViewMatrix();
 	void mouseMovement(const glm::vec2& pPos, bool pConstraintPitch = true);
-	void moveRight(float pSpeed);
-	void moveLeft(float pSpeed);
-	void moveForward(float pSpeed);
-	void moveBackward(float pSpeed);
-	void moveDown(float pSpeed);
-	void moveUp(float pSpeed);
+	void moveCamera(moveSides pMoveSide, float pSpeed);
 
+	void setMovementSpeed(float pMovementSpeed);
 	void setSensivity(float pSensivity);
-	float getSensivity() const noexcept;
-
 	void setYaw(float pYaw);
 	void setPitch(float pPitch);
+	float getMovementSpeed() const noexcept;
+	float getSensivity() const noexcept;
 	float getYaw() const noexcept;
 	float getPitch() const noexcept;
 
@@ -40,9 +46,10 @@ private:
 	void updateCameraVertex();
 
 private:
-	glm::vec3 mEye, mDirection, mUpVec;
+	float mYaw{}, mPitch{};
+	float mSensivity{};
+	float mMovementSpeed{};
 
-	float mYaw, mPitch;
-	float mSensivity;
+	glm::vec3 mEye, mDirection, mUpVec;
 };
 
