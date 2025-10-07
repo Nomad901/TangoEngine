48128d81ce4f6d095257665073069b85735d544b
Tango <maxim.ant07@gmail.com>
2025-10-07 21:20:19 +0300

    Update terrainVert.glsl

--- resources/Shaders/terrainVert.glsl
+++ resources/Shaders/terrainVert.glsl
@@ -7,9 +7,15 @@ layout(location = 3) in vec2 posTex;
 out vec4 Color;
 
 uniform mat4 uView;
+uniform mat4 uProj;
+uniform float uMinHeight;
+uniform float uMaxHeight;
 
 void main()
 {
-	gl_Position = uView * vec4(pos, 1.0f);
-	Color = vec4(pos.y / 200.0f);
+	gl_Position = uProj * uView * vec4(pos, 1.0f);
+	float deltaHeight = uMaxHeight - uMinHeight;
+	float heightRatio = (pos.y - uMinHeight) / deltaHeight;
+	float c = heightRatio * 0.8 + 0.2;
+	Color = vec4(c, c, c, 1.0f);
 }
