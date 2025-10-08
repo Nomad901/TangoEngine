2b7482f86dfe309e78772bc530928e079cc36df4
Tango <maxim.ant07@gmail.com>
2025-10-08 13:46:23 +0300

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -16,6 +16,11 @@ public:
 	std::vector<float> readFromBinaryFile2Float(const std::filesystem::path& pPath,
 												int64_t& pSize);
 
+	float randomFloatRange(float pStart, float pEnd);
+	float randomFloat();
+	
+	int32_t calcNextPowerOfTwo(int32_t pX);
+
 	Utils(const Utils&) = delete;
 	Utils& operator=(const Utils&) noexcept = delete;
 	Utils(Utils&&) = delete;
