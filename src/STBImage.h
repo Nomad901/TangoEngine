956fdaa08395849d83c953c319c411bf75128d3d
Tango <maxim.ant07@gmail.com>
2025-10-09 05:47:44 +0300

    Create STBImage.h

---
+++ src/STBImage.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <format>
+#include <filesystem>
+
+#include "glm/glm.hpp"
+#include "dependencies/stb_image.h"
+
+class STBImage
+{
+public:
+	STBImage() = default;
+	STBImage(const std::filesystem::path& pPath);
+	~STBImage();
+
+	void load(const std::filesystem::path& pPath);
+	void unload();
+
+	glm::vec3 getColor(int32_t pX, int32_t pY) const noexcept;
+
+private:
+	int32_t mWidth{}, mHeight{}, mBPP{};
+	uint8_t* mBufferData;
+};
+
