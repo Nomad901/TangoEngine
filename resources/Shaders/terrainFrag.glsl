ec351ee54ba814b2670ab52d5d755960727a3408
Tango <maxim.ant07@gmail.com>
2025-10-10 17:49:57 +0300

    Add files via upload

--- resources/Shaders/terrainFrag.glsl
+++ resources/Shaders/terrainFrag.glsl
@@ -3,10 +3,74 @@
 out vec4 fragColor;
 
 in vec4 Color;
+in vec2 TexCoord;
+in vec3 worldPos;
 
-uniform sampler2D uGrassTex;
+//
+// UNIFORMS TEXTURES
+//
+uniform sampler2D uTextureHeight0;
+uniform sampler2D uTextureHeight1;
+uniform sampler2D uTextureHeight2;
+uniform sampler2D uTextureHeight3;
+
+// 
+// UNIFORMS HEIGHTS
+//
+uniform float uHeight0 = 20.0f;
+uniform float uHeight1 = 50.0f;
+uniform float uHeight2 = 80.0f;
+uniform float uHeight3 = 130.0f;
+
+// 
+// UNIFORM CHECK-COLOR
+//
+uniform int isSingleTex;
+
+vec4 calculateTexColor()
+{
+		vec4 texColor;
+	float height = worldPos.y;
+
+	if (height < uHeight0)
+		texColor = texture(uTextureHeight0, TexCoord);
+	else if (height < uHeight1)
+	{
+		vec4 color0  = texture(uTextureHeight0, TexCoord);
+		vec4 color1  = texture(uTextureHeight1, TexCoord);
+		float delta  = uHeight1 - uHeight0;
+		float factor = (height - uHeight0) / delta;
+		texColor = mix(color0, color1, factor);
+	}
+	else if (height < uHeight2)
+	{
+		vec4 color0  = texture(uTextureHeight1, TexCoord);
+		vec4 color1  = texture(uTextureHeight2, TexCoord);
+		float delta  = uHeight2 - uHeight1;
+		float factor = (height - uHeight1) / delta;
+		texColor = mix(color0, color1, factor);
+	}
+	else if (height < uHeight3)
+	{
+		vec4 color0  = texture(uTextureHeight2, TexCoord);
+		vec4 color1  = texture(uTextureHeight3, TexCoord);
+		float delta  = uHeight3 - uHeight2;
+		float factor = (height - uHeight2) / delta;
+		texColor = mix(color0, color1, factor);
+	}
+	else 
+		texColor = texture(uTextureHeight3, TexCoord);
+	
+	return texColor;
+}
 
 void main()
 {
-	fragColor = Color;
+	if(isSingleTex == 1)
+		fragColor = Color;
+	else 
+	{
+		vec4 texture_ = calculateTexColor();
+		fragColor = Color * texture_;
+	}
 }
--- resources/Shaders/terrainVert.glsl
+++ resources/Shaders/terrainVert.glsl
@@ -5,17 +5,22 @@ layout(location = 2) in vec4 color;
 layout(location = 3) in vec2 posTex;
 
 out vec4 Color;
+out vec2 TexCoord;
+out vec3 worldPos;
 
 uniform mat4 uView;
 uniform mat4 uProj;
+uniform mat4 uModel;
 uniform float uMinHeight;
 uniform float uMaxHeight;
 
 void main()
 {
-	gl_Position = uProj * uView * vec4(pos, 1.0f);
+	gl_Position = uProj * uView * uModel * vec4(pos, 1.0f);
 	float deltaHeight = uMaxHeight - uMinHeight;
 	float heightRatio = (pos.y - uMinHeight) / deltaHeight;
 	float c = heightRatio * 0.8 + 0.2;
 	Color = vec4(c, c, c, 1.0f);
+	TexCoord = posTex;
+	worldPos = pos;
 }
