53e1e749c1808e3b7fede92b99ede82f8da9ca9a
Tango <maxim.ant07@gmail.com>
2025-10-18 20:19:30 +0300

    Update FrustumCulling.h

--- src/FrustumCulling.h
+++ src/FrustumCulling.h
@@ -2,26 +2,46 @@
 #include <array>
 
 #include "glm/glm.hpp"
+
 #include "Utils.h"
+#include "Camera.h"
+
+enum class PlaneType
+{
+	TOP_FACE = 0,
+	BOTTOM_FACE = 1,
+	LEFT_FACE = 2,
+	RIGHT_FACE = 3,
+	FAR_FACE = 4,
+	NEAR_FACE = 5
+};
 
 class FrustumCulling
 {
+private:
+	struct Plane;
 public:
-	FrustumCulling() = default;
-	FrustumCulling(const glm::mat4& pViewProjMat);
+	FrustumCulling();
+	FrustumCulling(const Camera& pCamera, float pAspect, float pFovY,
+				   float pZNear, float pZFar);
 
-	void update(const glm::mat4& pViewProjMat);
-
-	bool isPointInsideViewFrustum(const glm::vec3& pPoint) const;
-	//bool isAABBInsideViewFrustum(const glm::vec3& pMinBounds, 
-	//							 const glm::vec3& pMaxBounds) const;
+	void initFrustumFromCamera(const Camera& pCamera, float pAspect, float pFovY, 
+													  float pZNear, float pZFar);
+	Plane& getPlane(PlaneType pPlaneType) noexcept;
 
 private:
-	glm::vec4 mLeftClipPlane;
-	glm::vec4 mRightClipPlane;
-	glm::vec4 mTopClipPlane;
-	glm::vec4 mBottomClipPlane;
-	glm::vec4 mNearClipPlane;
-	glm::vec4 mFarClipPlane;
+	struct Plane
+	{
+		glm::vec3 mNormal{ 0.0f, 1.0f, 0.0f };
+		float mDistance{ 0.0f };
+		Plane() = default;
+		Plane(const glm::vec3& pPoint, const glm::vec3& pNormal);
+		float getSignedDistanceToPlane(const glm::vec3& pPoint) const;
+
+	};
+	
+private:
+	std::array<Plane, 6> mPlanes;
+
 };
 
