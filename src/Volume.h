19876d08849977defb46039683ca2d68f3581088
Tango <maxim.ant07@gmail.com>
2025-10-19 06:17:03 +0300

    Update Volume.h

--- src/Volume.h
+++ src/Volume.h
@@ -11,20 +11,38 @@ struct Volume
 	Volume() = default;
 	virtual ~Volume() = default;
 
-	virtual bool isOnFrustum(const FrustumCulling& pFrustumCulling, 
+	virtual bool isOnFrustum(FrustumCulling& pFrustumCulling, 
 							 const glm::mat4& pModelMatrix) = 0;
 };
 
 class Sphere : public Volume
 {
 public:
-	Sphere() = default;
+	Sphere(const glm::vec3& pCenter, float pRadius);
 
-	bool isOnFrustum(const FrustumCulling& pFrustumCulling,
+	bool isOnForwardPlane(const Plane& pPlane);
+	bool isOnFrustum(FrustumCulling& pFrustumCulling,
 					 const glm::mat4& pModelMatrix) override;
 	
 private:
 	glm::vec3 mCenter{ 0.0f };
 	float mRadius{ 0.0f };
 
+};
+
+class AABB : public Volume
+{
+public:
+	AABB(const glm::vec3& pMax, const glm::vec3& pMin);
+	AABB(const glm::vec3& pCenter, float pLX, float pLY, float pLZ);
+
+	std::array<glm::vec3, 8> getVertices() const;
+
+	bool isOnForwardPlane(const Plane& pPlane) const;
+	bool isOnFrustum(FrustumCulling& pFrustumCulling,
+					 const glm::mat4& pModelMatrix) override;
+
+private:
+	glm::vec3 mCenter{ 0.0f };
+	glm::vec3 mExtents{ 0.0f };
 };
