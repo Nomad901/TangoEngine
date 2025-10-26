8b6354418cd1f46d0499259e16d59d2e78fa7946
Tango <maxim.ant07@gmail.com>
2025-10-26 16:43:49 +0300

    Delete src/src directory

--- src/src/PhysicsEquations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-#include "glm.hpp"
-
-static float kinematicEquation(float pAcceleration, float pVelocity, float pPos, float pTime)
-{
-	return 0.5f * pAcceleration * pTime * pTime + pVelocity * pTime + pPos;
-}
--- src/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Program.h"
-
-int32_t main(int32_t argc, int8_t* argv[])
-{
-	Program program;
-	program.run();
-}
