0d00b09c84640610384ea6cd132024f4ffd573b3
Tango <maxim.ant07@gmail.com>
2025-11-07 19:56:56 +0300

    Add files via upload

--- resources/Shaders/skinMeshTestFrag.glsl
+++ resources/Shaders/skinMeshTestFrag.glsl
@@ -6,7 +6,9 @@ in vec3 fragPos;
 in vec2 fragTexCoord;
 in vec3 fragNormals;
 flat in vec4 fragBoneID;
+flat in vec2 fragBoneID2;
 in vec4 fragBoneWeights;
+in vec2 fragBoneWeights2;
 
 const int MAX_POINT_LIGHTS = 2;
 const int MAX_SPOT_LIGHTS = 2;
@@ -43,7 +45,7 @@ struct spotLight
 	pointLight mPointLight;
 	vec3 mDirection;
 	float mCutOff;
-}
+};
 
 struct material 
 {
@@ -58,7 +60,7 @@ uniform spotLight uSpotLight[MAX_SPOT_LIGHTS];
 uniform int uNimberPointLights;
 uniform int uNumberSpotLights;
 
-uniform Material uMaterial;
+uniform material uMaterial;
 
 uniform sampler2D uSampler;
 uniform sampler2D uSamplerSpecularComponent;
@@ -81,7 +83,7 @@ vec4 calcLightInternal(baseLight pBaseLight, vec3 pLightDirection, vec3 pNormal)
 
 	if (diffuseFactor > 0.0f)
 	{
-		diffuseColor = vec4(pBaseLight.color, 1.0f) * 
+		diffuseColor = vec4(pBaseLight.mColor, 1.0f) * 
 					   pBaseLight.mDiffuseIntensity *
 					   vec4(uMaterial.mDiffuseColor, 1.0f) * 
 					   diffuseFactor;
@@ -89,11 +91,11 @@ vec4 calcLightInternal(baseLight pBaseLight, vec3 pLightDirection, vec3 pNormal)
 		vec3 pixelToCamera = normalize(uCameraPos - fragPos);
 		vec3 lightReflect  = normalize(reflect(pLightDirection, pNormal));
 		float specularFactor = dot(pixelToCamera, lightReflect);
-		if (specularColor > 0.0f)
+		if (specularFactor > 0.0f)
 		{
-			float specularExponent = texture(uSamplerSpecularComponent, fragTexCoord).r * 255.0f;
-			specularFactor = pow(specularColor, specularExponent);
-			specularColor = vec4(pBaseLight.color, 1.0f) *
+			float specularExponent = texture(uSamplerSpecularComponent, fragTexCoord).r * 32.0f + 1.0f;
+			specularFactor = pow(specularFactor, specularExponent);
+			specularColor = vec4(pBaseLight.mColor, 1.0f) *
 							pBaseLight.mDiffuseIntensity * 
 							vec4(uMaterial.mSpecularColor, 1.0f) * 
 							specularFactor;
@@ -153,17 +155,34 @@ void main()
 	}
 
 	bool found = false;
+	
+	int boneIDs[6];
+	float boneWeights[6];
+
+	boneIDs[0] = int(fragBoneID.x);
+	boneIDs[1] = int(fragBoneID.y);
+	boneIDs[2] = int(fragBoneID.z);
+	boneIDs[3] = int(fragBoneID.w);
+	boneIDs[4] = int(fragBoneID2.x);
+	boneIDs[5] = int(fragBoneID2.y);
+
+	boneWeights[0] = fragBoneWeights.x;
+	boneWeights[1] = fragBoneWeights.y;
+	boneWeights[2] = fragBoneWeights.z;
+	boneWeights[3] = fragBoneWeights.w;
+	boneWeights[4] = fragBoneWeights2.x;
+	boneWeights[5] = fragBoneWeights2.y;
 
 	for(int i = 0; i < uNumberOfBones; ++i)
 	{
-		if(fragBoneID[i] >= uDisplayBoneIndex)
+		if(boneIDs[i] == uDisplayBoneIndex)
 		{
-			if(fragBoneWeights[i] >= 0.7f)
-				fragColor = vec4(1.0f, 0.0f, 0.0f, 0.0f) * fragBoneWeights[i];
-			else if(fragBoneWeights[i] >= 0.4f && fragBoneWeights[i] <= 0.6f)
-				fragColor = vec4(0.0f, 1.0f, 0.0f, 0.0f) * fragBoneWeights[i];
-			else if(fragBoneWeights[i] >= 0.1f)
-				fragColor = vec4(1.0f, 1.0f, 0.0f, 0.0f) * fragBoneWeights[i];
+			if(boneWeights[i] >= 0.7f)
+				fragColor = vec4(1.0f, 0.0f, 0.0f, 0.0f) * boneWeights[i];
+			else if(boneWeights[i] >= 0.4f && boneWeights[i] <= 0.7f)
+				fragColor = vec4(0.0f, 1.0f, 0.0f, 0.0f) * boneWeights[i];
+			else if(boneWeights[i] >= 0.0f)
+				fragColor = vec4(1.0f, 1.0f, 0.0f, 0.0f) * boneWeights[i];
 			
 			found = true;
 			break;
@@ -172,6 +191,6 @@ void main()
 
 	if(!found)
 	{
-		fragColor = texture(uSampler, fragTexCoord) * totalLight * vec4(0.0001f) + vec4(0.0f, 0.0f, 1.0f, 0.0f);
+		fragColor = texture(uSampler, fragTexCoord) * totalLight * vec4(0.0001f) + vec4(0.0f, 0.0f, 1.0f, 1.0f);
 	}
 }
--- resources/Shaders/skinMeshTestVert.glsl
+++ resources/Shaders/skinMeshTestVert.glsl
@@ -3,13 +3,17 @@ layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 texCoord;
 layout (location = 2) in vec3 normals;
 layout (location = 3) in ivec4 boneID;
-layout (location = 4) in vec4 boneWeights;
+layout (location = 4) in ivec2 boneID2;
+layout (location = 5) in vec4 boneWeights;
+layout (location = 6) in vec2 boneWeights2;
 
 out vec3 fragPos;
 out vec2 fragTexCoord;
 out vec3 fragNormals;
-out vec4 fragBoneID;
+flat out vec4 fragBoneID;
+flat out vec2 fragBoneID2;
 out vec4 fragBoneWeights;
+out vec2 fragBoneWeights2;
 
 uniform mat4 uMVP;
 
@@ -21,5 +25,7 @@ void main()
 	fragTexCoord = texCoord;
 	fragNormals = normals;
 	fragBoneID = vec4(boneID);
+	fragBoneID2 = vec2(boneID2);
 	fragBoneWeights = boneWeights;
+	fragBoneWeights2 = boneWeights2;
 }
