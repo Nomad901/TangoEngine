e6ca84009dec3e0d0d6c69225f4a002fed5c6af4
Tango <maxim.ant07@gmail.com>
2025-11-16 17:52:53 +0300

    loger

---
+++ src/Log.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <format>
+#include <filesystem>
+#include <string>
+
+#include "Singleton.h"
+
+class Log 
+{
+public:
+	enum class TypeOfProblem;
+public:
+	void log(std::string_view pMessage,
+			 TypeOfProblem pTypeOfProblem,
+			 const std::filesystem::path& pFilePath = __FILE__,
+			 int32_t pLine = __LINE__);
+private:
+	Log() = default;
+	~Log() = default;
+	Log(const Log&) = delete;
+	Log& operator=(const Log&) = delete;
+	Log(Log&&) = delete;
+	Log& operator=(Log&&) = delete;
+
+	void manageWarning(std::string_view pMessage, 
+					   const std::filesystem::path& pFilePath,
+					   int32_t pLine);
+	void manageWarning(std::string_view pMessage,
+					   const std::filesystem::path& pFilePath,
+					   int32_t pLine);
+	void manageWarning(std::string_view pMessage,
+					   const std::filesystem::path& pFilePath,
+					   int32_t pLine);
+
+private:
+	enum class TypeOfProblem
+	{
+		WARNING = 0,
+		ERROR = 1,
+		CRITICAL_ERROR = 2
+	};
+};
+
